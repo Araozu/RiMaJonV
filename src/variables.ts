591eff4e9cb763506a179ff50a47c2bc5e917780
Araozu <faraoz@unsa.edu.pe>
2020-10-07 08:30:05 -0500

    Habilitado contador de cartas hasta sig bonus

--- src/variables.ts
+++ src/variables.ts
@@ -1,5 +1,4 @@
-export const servidor = process.env.SERVIDOR ?? "0.0.0.0:8080"; // "rimajonb.araozu.dev"; // "0.0.0.0:8080"; //
-export const servidorF = process.env.SERVIDORF ?? `http://${servidor}`;
-export const wsServidor = process.env.SERVIDORWS ?? `ws://${servidor}`;
+export const servidor = "rimajonb.araozu.dev"; // "0.0.0.0:8080"; //
+export const servidorF = `https://${servidor}`;
+export const wsServidor = `wss://${servidor}`;
 
-console.log(servidor, servidorF, wsServidor);
