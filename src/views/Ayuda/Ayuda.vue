28bd8f286bfe6cc6ce78493802753da0341b4af4
Araozu <faraoz@unsa.edu.pe>
2020-12-25 19:00:28 -0500

    Agregar 2 nuevos yaku y ajustar explicacion de otros

--- src/views/Ayuda/Ayuda.vue
+++ src/views/Ayuda/Ayuda.vue
@@ -8,147 +8,6 @@ div
 
     carta(v-for="(c, i) in cartas" :valor="c" :key="i")
 
-    p 4 cartas de los números 1 al 10 de color negro y rojo, 4 cartas de dragones de 4 colores, 4 J, Q y K.
-
-    h2 Mano
-    p Cada mano se compone de 10 + 1 cartas aleatorias.
-    grupo-cartas(:cartas="cartasR")
-
-    h2 Flujo del juego
-    p.
-        Hay 4 jugadores, cada uno con una mano de 10 cartas. En cada turno el jugador extrae una carta
-        y la une a su mano. Luego, debe descartar 1 carta.
-    p El objetivo del juego es formar una mano con 3 grupos y 1 par de cartas.
-
-    h2 Par de cartas
-    p Un par son 2 cartas del mismo color y valor.
-    grupo-cartas(:cartas="parCartasR")
-
-    h2 Grupo de cartas
-    p Un grupo de cartas puede ser cualquiera de los siguientes:
-    div(style="padding-left: 2rem")
-        h3 Secuencia
-        p 3 cartas del mismo color y con números consecutivos
-        grupo-cartas(:cartas="seqCartasR")
-        p J, Q, K y los dragones no pueden formar sequencias.
-        p Los números deben ser consecutivos: 1 2 3 es válido, pero 9 10 1 no lo es.
-
-        h3 Triple
-        p 3 cartas del mismo color y valor.
-        grupo-cartas(:cartas="triCartasR")
-
-        h3 Cuádruple
-        p 4 cartas del mismo color y valor.
-        grupo-cartas(:cartas="cuaCartasR")
-        p Cuando se forma un cuádruple se agrega una carta a la mano. Los cuadruples se consideran triples para el
-            | resto de efectos.
-
-    h2 Mano lista
-    p Una mano está lista cuando solo le falta 1 carta para ganar
-
-    grupo-cartas(:cartas="[10, 10, 11, 34, 36, 38, 38, 38, 128, 128]")
-    p.
-        Por ejemplo, la mano anterior está lista, porque solo le falta un dragon verde
-        para completar sus 3 grupos y 1 par.
-    p.
-        En esa mano, el primer grupo es un triple negro, el segundo grupo una secuencia de A-2-3 rojo, el
-        par son dos 3 rojos, y el último grupo sería un triple de dragon verde.
-
-    h2 Robar cartas
-    p Puedes robar cartas que tus oponentes descartan para formar tu mano en varias situaciones:
-
-    div.pad
-        h3 Robar para formar una secuencia
-        p.
-            Si tu oponente a tu izquierda descarta una carta que necesitas para formar una secuencia,
-            puedes robarla y formar tu secuencia.
-        p Por ejemplo, a las siguientes dos cartas les falta un 3 o 6 rojo para formar una secuencia.
-        grupo-cartas(:cartas="[40, 42]")
-        br
-        p Al siguiente par le falta un 6 negro para formar una secuencia.
-        grupo-cartas(:cartas="[10, 14]")
-
-        h3 Robar para formar un triple
-        p.
-            Si algún oponente descarta una carta que necesitas para formar un triple,
-            puedes robarla.
-        p Por ejemplo, al siguiente par le falta una J verde para formar un triple
-        grupo-cartas(:cartas="[192, 192]")
-
-        h3 Robar para formar un cuádruple
-        p.
-            Si algún oponente descarta una carta que necesitas para formar un cuádruple,
-            puedes robarla.
-        p Por ejemplo. al siguiente triple le falta un dragon azul para formar un cuádruple.
-        grupo-cartas(:cartas="[160, 160, 160]")
-
-        h3 Robar para ganar
-        p.
-            Si tu mano está lista, y algún oponente descarta la carta que necesitas para ganar,
-            puedes robarla. No importa si hacerlo forma una secuencia, triple, cuádruple o par.
-
-        h3 Prioridad de los robos
-        p.
-            Existe la posibilidad de que varios jugadores puedan robar una carta a la vez.
-            Si esto sucede la prioridad es la siguiente:
-        ol
-            li Robar para ganar
-            li Robar para cuádruple
-            li Robar para triple
-            li Robar para secuencia
-
-        h3 Consecuencias del robo
-        p Cuando robas una carta suceden 2 cosas:
-        ul
-            li Todos pueden ver la secuencia/triple/cuádruple que formaste
-            li No puedes cambiar esa secuencia/triple/cuádruple
-        p Adicionalmente, al robar una carta tu mano se considera "abierta"
-
-    h2 Mano abierta y cerrada
-    p Se dice que tu mano está abierta cuando tus oponentes pueden ver uno o más grupos de tu mano.
-    p Robar una carta para ganar no cuenta como abrir tu mano.
-
-    h2 Restricciones para ganar
-    ol
-        li No puedes ganar robando una carta que descartaste. No importa cuando.
-
-    h2 Indicadores de bonus
-    div
-        p Todas las partidas tienen 10 cartas reservadas que todos pueden ver. Estos son los indicadores de bonus.
-        grupo-cartas(:cartas="[0, 0, 0, 0, 0]")
-        br
-        grupo-cartas(:cartas="[0, 0, 0, 0, 0]")
-
-        p Al inicio de la partida se revela el primer indicador:
-        grupo-cartas(:cartas="[14, 0, 0, 0, 0]")
-        br
-        grupo-cartas(:cartas="[0, 0, 0, 0, 0]")
-
-        p Los siguientes 4 indicadores se revelan despues de 32/16/8/4 turnos.
-        p Los últimos 5 indicadores se revelan cada vez que alguien declara un cuádruple, uno a la vez.
-
-        div.pad
-            h3 Funcionamiento
-
-            p Cada carta igual al indicador vale 0.2 puntos extra.
-
-            h3 Indicadores y bonus repetidos
-            p Si algún bonus se repite se aumenta su valor.
-
-
-    h2 Indicadores de dragones
-    div
-        p.
-            A cada partida se le asigna un color, y a cada jugador se le asigna otro. Esos colores indican
-            qué triple/cuáduple de dragones brindan 1 punto adicional.
-
-        p.
-            Por ejemplo, si la partida es de color rojo, y tu eres de color verde, entonces
-            cualquier triple/cuádruple del dragon rojo o verde da 3 puntos adicionales.
-
-        grupo-cartas(:cartas="[96, 96, 96, -1, 128, 128, 128]")
-
-
     h2 Puntaje
     p Cada mano completa vale una cantidad de puntos según las combinaciones de cartas que posee.
 
@@ -164,6 +23,11 @@ div
                     | Doble secuencia
                 p 2 secuencias iguales del mismo color en mano cerrada.
                 grupo-cartas(:cartas="[2, 2, 4, 5, 6, 7, 44, 45, 44, 128, 128]")
+
+            div.yaku
+                h4 Variedad
+                p Al menos una carta de cada tipo
+                grupo-cartas(:cartas="[10, 11, 35, 36, 39, 160, 160, 160, 192, 192, 192]")
 
             div.yaku
                 h4 Realeza
@@ -190,6 +54,13 @@ div
         div.pad
 
             div.yaku
+                h4
+                    i.ph-lock-bold.img-lock(title="Solo en mano cerrada")
+                    | Doble secuencia pura
+                p 2 secuencias iguales del mismo color y mismo simbolo en mano cerrada.
+                grupo-cartas(:cartas="[2, 2, 4, 4, 6, 6, 44, 45, 44, 128, 128]")
+
+            div.yaku
                 h4 Semi exterior
                 p Cada par o grupo contiene al menos un 1, 10, J, Q, K o dragón.
                 grupo-cartas(:cartas="[2, 4, 7, 20, 20, 21, 48, 50, 52, 192, 192]")
@@ -206,13 +77,8 @@ div
 
             div.yaku
                 h4 Escalera
-                p 1,2,3,4,5,6,7,8,9 del mismo color.
+                p Cartas del 1 al 9 o del 2 al 10 del mismo color.
                 grupo-cartas(:cartas="[2, 4, 7, 9, 10, 12, 15, 16, 19, 128, 128]")
-
-            div.yaku
-                h4 Escalera
-                p 2,3,4,5,6,7,8,9,10 del mismo color.
-                grupo-cartas(:cartas="[5, 7, 9, 10, 12, 14, 17, 18, 20, 41, 41]")
 
         h3 3 puntos
 
@@ -236,13 +102,7 @@ div
         div.pad
             div.yaku
                 h4 Escalera full
-                p 1,1,2,3,4,5,6,7,8,9,10 del mismo color
-                grupo-cartas(:cartas="[2, 3, 4, 7, 9, 10, 13, 14, 17, 19, 21]")
-                p No se acumula con "escalera", "negro" o "rojo".
-
-            div.yaku
-                h4 Escalera full
-                p 1,2,3,4,5,6,7,8,9,10,10 del mismo color
+                p Una escalera y un par del numero faltante del mismo color
                 grupo-cartas(:cartas="[35, 37, 38, 40, 42, 45, 47, 48, 51, 52, 52]")
                 p No se acumula con "escalera", "negro" o "rojo".
 
