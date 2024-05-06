c9563fe611113b5ad35fa28736be9ff83103993d
Araozu <fernando@araozu.dev>
2024-05-06 16:13:10 -0500

    Add /ws prefix to websockets

--- src/variables.ts
+++ src/variables.ts
@@ -1,3 +1,3 @@
 export const servidorF = import.meta.env.VITE_BACKEND_URL + "/api";
-export const wsServidor = import.meta.env.VITE_WS_URL + "/api";
+export const wsServidor = import.meta.env.VITE_WS_URL + "/ws";
 
